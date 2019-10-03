134f5759369d4c8f49445549c587c6219074522b
DANIEL PHILLIPS <V8056288@live.tees.ac.uk>
2019-10-03 14:53:28 +0100

    Added the Employee class and ToString Mehod.

---
+++ EmployeeManager/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManager
+{
+    class Employee
+    {
+        public Employee(string eName, int eID, int eHoursWorked, int eHourlyRate)
+        {
+            EName = eName;
+            EID = eID;
+            EHoursWorked = eHoursWorked;
+            EHourlyRate = eHourlyRate;
+            
+        }
+
+        
+        public string ToString(Employee e)
+        {
+            string ToString = (e.EName + " " + e.EID);
+
+            return ToString;
+        }
+
+        public string EName { get; set; }
+        public int EID { get; set; }
+        public int EHoursWorked { get; set; }
+        public int EHourlyRate { get; set; }
+
+    }
+}
